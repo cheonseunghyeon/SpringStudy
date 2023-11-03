163e8ba33c1bf131a863f4d0cfcb792da5523454
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-11-03 11:05:57 +0900

    3주차 학습 내용

--- Assignment/CHE/helloboot/src/main/java/tobyspring/helloboot/SpringApplication.java
+++ Assignment/CHE/helloboot/src/main/java/tobyspring/helloboot/SpringApplication.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,20 +16,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class SpringApplication {
+
+public class HellobootApplication {
+
 	public static void main(String[] args) {
+
 		ServletWebServerFactory serverFactory= new TomcatServletWebServerFactory();
         WebServer webServer=serverFactory.getWebServer(servletContext -> {
 			HelloController helloController=new HelloController();
             servletContext.addServlet("frontcontroller", new HttpServlet() {
+
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
+						
 						String ret=helloController.hello(name);
-						/
+
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello Servlet");
 						resp.getWriter().println(ret);
 					}
 					else if (req.getRequestURI().equals("/user")){
