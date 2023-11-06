bf89de82657a1f2a153a3322ef1be262c02dc59b
SunMi <wywudi@naver.com>
2023-11-06 22:17:32 +0900

    feat(part.09) Spring JDBC 자동 구성 개발

---
+++ Training/sunmi/spring_study/src/main/java/com/example/spring_study/AutoConfig/ServerProperties.java
@@ -0,0 +1,23 @@
+package com.example.spring_study.AutoConfig;
+
+public class ServerProperties {
+    private String contextPath;
+
+    private int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
