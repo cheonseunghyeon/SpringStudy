a1d361c81c2e19a8df2df1f97925382287e038f8
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-12-23 19:06:28 +0900

    Part 7 -  7. 조건부 자동 구성(@Conditonal)

--- Assignment/CHE/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ Assignment/CHE/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,15 +2,17 @@ package tobyspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
-
 }
