5aeb5f571ebea16fc9ed37daf06fb866e54636a0
userNameBHL <127075672+userNameBHL@users.noreply.github.com>
2023-12-23 17:24:19 +0900

    part10
    part10

---
+++ Assignment/BHL/helloboot/helloboot/src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,25 @@
+package tobyspring.helloboot;
+
+
+@HellobootTest
+public class HelloRepositoryTest {
+ @Autowired JdbcTemplate jdbcTemplate;
+    @Autowired HelloRepository helloRepository;
+
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("Toby")).isNull();
+    }
+
+    @Test
+    void incraseCount() {
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(0);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(1);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(2);
+    }
+
+}
---
+++ Assignment/BHL/helloboot/helloboot/src/test/java/tobyspring/helloboot/HelloServiceCountTest.java
@@ -0,0 +1,15 @@
+package tobyspring.helloboot;
+
+@HellobootTest
+public class HelloServiceCountTest {
+ @Autowired HelloService helloService;
+    @Autowired HelloRepository helloRepository;
+
+    @Test
+    void sayHelloIncreaseCount() {
+        IntStream.rangeClosed(1, 10).forEach(count -> {
+            helloService.sayHello("Toby");
+            Assertions.assertThat(helloRepository.countOf("Toby")).isEqualTo(count);
+        });
+    }
+}
