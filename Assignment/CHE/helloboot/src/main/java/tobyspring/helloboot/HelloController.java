87a9ae9c8a221d0c742011e3b7a152e341669f09
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-12-23 19:11:33 +0900

    Part 10 - 스프링 부트 자세히 살펴보기

--- Assignment/CHE/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ Assignment/CHE/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,10 +1,7 @@
 package tobyspring.helloboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 @RestController
 public class HelloController {
@@ -19,5 +16,10 @@ public class HelloController {
         if (name == null || name.trim().length() == 0) throw new IllegalArgumentException();
 
         return helloService.sayHello(name);
+    }
+
+    @GetMapping("/count")
+    public String count(String name) {
+        return name + ": " + helloService.countOf(name);
     }
 }
